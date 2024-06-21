423641da3ffc52ec8530bec02ecd3dddd4a892f8
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2024-06-21 22:35:22 +0530

    Create DigitsInFactorial.java
    Given an integer N. Find the number of digits that appear in its factorial. 
    Factorial is defined as, factorial(n) = 1*2*3*4……..*N and factorial(0) = 1.

---
+++ Basic/DigitsInFactorial.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.BigDecimal; 
+
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int T=sc.nextInt();
+		
+		while(T-->0)
+		{
+		    Solution obj=new Solution();
+		    int N;
+		    
+		    //taking N
+		    N=sc.nextInt();
+		    
+		   //calling method digitsInFactorial()
+		   System.out.println(obj.digitsInFactorial(N));
+		    
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public int digitsInFactorial(int N){
+        if (N == 1)
+            return 1;
+        
+        double digits = 0;
+        for(int i = 2; i<=N; i++)
+        {
+            digits += Math.log10(i);
+        }
+        return (int)Math.floor(digits)+1;
+    }
+}
