82e4d198f76b9b78f1fb81dcb2f438c68169eb5e
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2024-07-22 20:25:47 +0530

    Create Power.java   Difficulty:(Basic)
    You are given two numbers n and p. You need to find n^p.
    
    our Task:
    You don't need to read input or print anything. You only need to complete the function RecursivePower() that takes n and p as parameters and returns np.
    
    Expected Time Complexity: O(p).
    Expected Auxiliary Space: O(p).

---
+++ Recursion/Power.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driver_class
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t  = sc.nextInt();//testcases
+        
+        while(t-- > 0)
+        {
+            //input n and p
+            int n = sc.nextInt();
+            int p = sc.nextInt();
+            
+            //calling RecursivePower() method 
+            System.out.println(new Solution().RecursivePower(n,p));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int RecursivePower(int n,int p)
+    {
+        if(p==1)
+            return n;
+            
+        if(p==0)
+            return 1;
+        return (n*RecursivePower(n,p-1));
+    }
+ 
+}
