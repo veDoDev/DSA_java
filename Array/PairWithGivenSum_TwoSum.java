e57c97b49d629d45aa4036768cb223f7dfad24ed
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2024-11-04 16:00:00 +0530

    Create PairWithGivenSum_TwoSum.java       Difficulty : Easy
    Given an array arr of positive integers and another number target. Determine whether two elements exist in arr whose sum is exactly target or not. Return a boolean value true if two elements exist in arr else return false.
    
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(n)
    
    Constraints:
    1 ≤ arr.size ≤ 105
    1 ≤ arr[i] ≤ 105
    
    gfg link : https://www.geeksforgeeks.org/problems/key-pair5616/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card

---
+++ Array/PairWithGivenSum_TwoSum.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int nums[], int target)
+    {
+        HashSet<Integer> dir = new HashSet<>();
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(dir.contains(target-nums[i]))   return true;
+            else    dir.add(nums[i]);
+        }
+        return false;
+    }
+}
