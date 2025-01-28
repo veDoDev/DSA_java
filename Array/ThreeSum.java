8338cf07b5ee548b556da3fe78e29eee4e84159a
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2025-01-28 23:12:39 +0530

    Create ThreeSum.java                   Difficulty : Medium
    GFG link : https://www.geeksforgeeks.org/problems/three-sum/0
    
    leetcode link : https://leetcode.com/problems/3sum/description/
    
    description : 
    Given an integer array arr, return all the unique triplets [arr[i], arr[j], arr[k]] such that i != j, i != k, and j != k, and arr[i] + arr[j] + arr[k] == 0.
    
    Note: The triplets must be returned in sorted order, the solution vector should also be sorted, and the answer must not contain any duplicate triplets.
    
    Expected Time Complexity: O(n^2)
    Expected Auxiliary Space: O(n^2)
    
    Constraints : 
    3 <= arr.length <= 3000
    -10^5 <= arr[i] <= 10^5
    
    Topic Tags : 
    sliding-window, Sorting, two-pointer-algorithm

---
+++ Array/ThreeSum.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class IntMatrix {
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException {
+        int[][] mat = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for (int j = 0; j < s.length; j++) mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> res = obj.triplets(arr);
+            if (res.size() == 0) {
+                System.out.println();
+            } else {
+                IntMatrix.print(res);
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> triplets(int[] arr) {
+        int n = arr.length;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 2; i++) 
+        {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) 
+            {
+                int sum = arr[i] + arr[j] + arr[k];
+
+                if (sum < 0)    j++;
+                else if (sum > 0)   k--;
+                else 
+                {
+                    res.add(new ArrayList<>(Arrays.asList(arr[i], arr[j], arr[k])));
+
+                    while (j < k && arr[j] == arr[j + 1]) j++;
+                    while (j < k && arr[k] == arr[k - 1]) k--;
+
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return res;
+    }
+}
