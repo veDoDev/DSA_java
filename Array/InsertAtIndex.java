49f2465a8fa39e4bcc671c887b2d4f3eb073470b
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2025-01-29 13:16:15 +0530

    Create InsertAtIndex.java              Difficulty : Basic
    link : https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/problem/array-insert-at-index
    
    Description : Insertion is a basic but frequently used operation. Arrays in most languages cannnot be dynamically shrinked or expanded. Here, we will work with such arrays and try to insert an element at some index.
    
    You are given an array arr(0-based index). The size of the array is given by sizeOfArray. You need to insert an element at given index.
    
    Your Task:
    You don't need to read input or print anything.. The input is already taken care of by the driver code. You only need to complete the function insertAtIndex() that takes arr, sizeOfArray, index, element as input and modifies the array arr as per requirements. The printing is done by driver code.
    
    Constraints:
    1 <= sizeOfArray <= 10000
    0 <= element, arri <= 10^6
    0 <= index <= sizeOfArray-1
    
    Topic Tags : 
    ArraysData Structures

---
+++ Array/InsertAtIndex.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Array {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Input testcases
+		int testcases = sc.nextInt();
+		while(testcases-- > 0)
+		{
+		    //input size of Array
+		    int sizeOfArray = sc.nextInt();
+		    int arr[] = new int[sizeOfArray];
+		    
+		    //input elements in the Array
+		    for(int i = 0; i < sizeOfArray-1; i++)
+		    {
+		        int x;
+		        x = sc.nextInt();
+		        arr[i] = x;
+		    }
+		    
+		    //input index
+		    int index = sc.nextInt();
+		    //input element
+		    int element = sc.nextInt();
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling insertAtIndex() function
+		    obj.insertAtIndex(arr,sizeOfArray,index,element);
+		    
+		    //Printing the array for verification
+		    for(int i = 0; i < sizeOfArray;i++)
+		    {
+		        System.out.print(arr[i] + " ");
+		        
+		    }
+		    
+		    System.out.println();
+		
+System.out.println("~");
+}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//Complete this function, Geeks
+class Solution
+{
+    public void insertAtIndex(int arr[],int sizeOfArray,int index,int element)
+    {
+        for(int i = sizeOfArray-1; i >= index; i--)
+        {
+            if(i == index)  arr[i] = element;
+            else    arr[i] = arr[i-1];
+        }
+        return;
+    }
+
+
+}
