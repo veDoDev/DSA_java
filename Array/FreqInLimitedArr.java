10a00e153293e56c3d2f3b41f7626b507b88e91b
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2025-02-27 17:39:19 +0530

    Create FreqInLimitedArr.java                          Difficulty : Medium
    link : https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/problem/frequency-of-array-elements-1587115620
    
    description : 
    You are given an array arr[] containing positive integers. The elements in the array arr[] range from 1 to n (where n is the size of the array), and some numbers may be repeated or absent. Your task is to count the frequency of all numbers in the range 1 to n and return an array of size n such that result[i] represents the frequency of the number i (1-based indexing).
    
    Constraints:
    1 ≤ arr.size() ≤ 106
    1 ≤ arr[i] ≤ arr.size()
    
    Company Tags
    Paytm, VMWare, Zoho, Amazon, Microsoft
    
    Topic Tags
    Arrays, Hash, Data Structures

---
+++ Array/FreqInLimitedArr.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().frequencyCount(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count the frequency of all elements from 1 to N in the array.
+    public List<Integer> frequencyCount(int[] arr) 
+    {
+        int n = arr.length;
+        List<Integer> flag_arr = new ArrayList<>(Collections.nCopies(n, 0));
+        
+        for(int i = 0; i < n; i++)
+        {
+            flag_arr.set(arr[i]-1,flag_arr.get(arr[i]-1)+1);
+        }
+        return flag_arr;
+    }
+}
