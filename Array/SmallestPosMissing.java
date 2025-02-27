8db0730ec3d102f65370e52a8509edfa21c619e5
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2025-02-27 16:30:07 +0530

    Create SmallestPosMissing.java                         Difficulty: Medium
    link : https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/problem/smallest-positive-missing-number-1587115621
    
    description : 
    You are given an integer array arr[]. Your task is to find the smallest positive number missing from the array.
    
    Note: Positive number starts from 1. The array can have negative integers too.
    
    Constraints:  
    1 <= arr.size() <= 105
    -106 <= arr[i] <= 106
    
    Company Tags
    Accolite, Amazon, Samsung, Snapdeal
    
    Topic Tags
    Arrays, Searching, Data Structures, Algorithms

---
+++ Array/SmallestPosMissing.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Reading number of test cases (t)
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // Read the line of integers (prices)
+            String input = br.readLine().trim();
+
+            // Split the input line into integers and store in an array
+            String[] tokens = input.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            // Create an instance of the Solution class
+            Solution ob = new Solution();
+
+            // Call the missingNumber method
+            int res = ob.missingNumber(arr);
+
+            // Print the result
+            System.out.println(res);
+
+            // Print the "~" symbol to match the original output
+            // System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) 
+    {
+        int n = arr.length, max = 0;
+        
+        for(int i = 0; i < n; i++)  if(arr[i] > max)    max = arr[i];
+        
+        boolean[] flag_arr = new boolean[max+1];
+        
+        for(int i = 0; i < n; i++)  if(arr[i] > 0)  flag_arr[arr[i]] = true;
+        
+        for(int i = 1; i < flag_arr.length; i++)  if(!flag_arr[i])  return i;
+        
+        return flag_arr.length;
+        
+    }
+}
